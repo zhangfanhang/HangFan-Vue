c541bf233288c71563bf6e380d7e53ffc5697802
zhangfanhang <543629463@qq.com>
2022-11-28 12:01:44 +0800

    线上问题修复

--- HangFan-Vue-Docs/docs/.vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"HangFan-Vue\",\"description\":\"一个由Vue 3 + Vue-Router + Element-Plus + Pinia打造的后台管理系统模版\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/logo.png\"}]],\"locales\":{}}")
--- HangFan-Vue-Docs/docs/.vuepress/.temp/theme-hope/sidebar.js
+++
@@ -1,1 +0,0 @@
-export const sidebarData = {"/":["README.md","说明.md","版本日志.md"]};
